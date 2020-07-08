a9c4fea3f0e6c3fbc8ad0aad68633779957bd061
0ishan0 <ishanbagchi4@gmail.com>
2020-07-09 00:21:25 +0530

    add fireflies background animation

---
+++ js/fireflies.js
@@ -0,0 +1,15 @@
+document.body.innerHTML = "<div class='bg'></div><div class='bg'></div><div class='bg'></div><div class='bg'></div><div class='bg'></div><div class='bg'></div><div class='bg'></div><div class='bg'></div>" + document.body.innerHTML;
+
+// Get HTML head element 
+var head = document.getElementsByTagName('HEAD')[0];  
+
+// Create new link Element 
+var link = document.createElement('link'); 
+
+// set the attributes for link element  
+link.rel = 'stylesheet';  
+link.type = 'text/css'; 
+link.href = 'https://ishanbagchi.github.io/backgrounds/css/noise-transparent.css';  
+
+// Append link element to HTML head 
+head.appendChild(link);  
