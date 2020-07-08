c7b511e5f3069351a3d8be7095d726a9e0e2bbb8
0ishan0 <ishanbagchi4@gmail.com>
2020-07-09 00:07:16 +0530

    adjust link tag

--- js/noise-transparent.js
+++ js/noise-transparent.js
@@ -9,7 +9,7 @@ var link = document.createElement('link');
 // set the attributes for link element  
 link.rel = 'stylesheet';  
 link.type = 'text/css'; 
-link.href = '../css/noise-transparent.css';  
+link.href = 'https://ishanbagchi.github.io/backgrounds/css/noise-transparent.css';  
 
 // Append link element to HTML head 
 head.appendChild(link);  
